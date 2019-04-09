6be7d2794fbdd5f29473584199f973a1b0ccef2f
Reid Wheeler <rwheeler9@hawkmail.hccfl.edu>
2019-04-08 23:03:30 -0400

    Added two test classes: AccountTest and CustomerTest

--- src/test/java/com/glooble/banking/AccountTest.java
+++ src/test/java/com/glooble/banking/AccountTest.java
@@ -5,10 +5,130 @@
  */
 package com.glooble.banking;
 
+import org.junit.Test;
+
 /**
  *
  * @author Reid
  */
 public class AccountTest {
+
+    /**
+     * Test of deposit method, of class Account.
+     */
+    @Test
+    public void testDeposit() {
+    }
+
+    /**
+     * Test of withdraw method, of class Account.
+     */
+    @Test
+    public void testWithdraw() {
+    }
+
+    /**
+     * Test of transfer method, of class Account.
+     */
+    @Test
+    public void testTransfer() {
+    }
+
+    /**
+     * Test of getAccountDescription method, of class Account.
+     */
+    @Test
+    public void testGetAccountDescription() {
+    }
+
+    /**
+     * Test of setAccountDescription method, of class Account.
+     */
+    @Test
+    public void testSetAccountDescription() {
+    }
+
+    /**
+     * Test of getBalance method, of class Account.
+     */
+    @Test
+    public void testGetBalance() {
+    }
+
+    /**
+     * Test of getTransactions method, of class Account.
+     */
+    @Test
+    public void testGetTransactions() {
+    }
+
+    /**
+     * Test of getTransaction method, of class Account.
+     */
+    @Test
+    public void testGetTransaction() {
+    }
+
+    /**
+     * Test of getAccountId method, of class Account.
+     */
+    @Test
+    public void testGetAccountId() {
+    }
+
+    /**
+     * Test of getCustomerId method, of class Account.
+     */
+    @Test
+    public void testGetCustomerId() {
+    }
+
+    /**
+     * Test of getAccountCreationDate method, of class Account.
+     */
+    @Test
+    public void testGetAccountCreationDate() {
+    }
+
+    /**
+     * Test of hashCode method, of class Account.
+     */
+    @Test
+    public void testHashCode() {
+    }
+
+    /**
+     * Test of equals method, of class Account.
+     */
+    @Test
+    public void testEquals() {
+    }
+
+    /**
+     * Test of compareTo method, of class Account.
+     */
+    @Test
+    public void testCompareTo() {
+    }
+
+    /**
+     * Test of toString method, of class Account.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    public class AccountImpl extends Account {
+
+        public AccountImpl() {
+            super(null, 0.0, "");
+        }
+
+        public void deposit(double amount) {
+        }
+
+        public void withdraw(double amount) {
+        }
+    }
     
 }
---
+++ src/test/java/com/glooble/banking/CustomerTest.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.glooble.banking;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Reid
+ */
+public class CustomerTest {
+    
+    public CustomerTest() {
+    }
+
+    /**
+     * Test of getBank method, of class Customer.
+     */
+    @Test
+    public void testGetBank() {
+    }
+
+    /**
+     * Test of getCustomerId method, of class Customer.
+     */
+    @Test
+    public void testGetCustomerId() {
+    }
+
+    /**
+     * Test of getLastName method, of class Customer.
+     */
+    @Test
+    public void testGetLastName() {
+    }
+
+    /**
+     * Test of getFirstName method, of class Customer.
+     */
+    @Test
+    public void testGetFirstName() {
+    }
+
+    /**
+     * Test of getCustomerAccounts method, of class Customer.
+     */
+    @Test
+    public void testGetCustomerAccounts() {
+    }
+
+    /**
+     * Test of ytdFees method, of class Customer.
+     */
+    @Test
+    public void testYtdFees() {
+    }
+
+    /**
+     * Test of ytdInterest method, of class Customer.
+     */
+    @Test
+    public void testYtdInterest() {
+    }
+
+    /**
+     * Test of addSavingsAccount method, of class Customer.
+     */
+    @Test
+    public void testAddSavingsAccount() {
+    }
+
+    /**
+     * Test of removeAccount method, of class Customer.
+     */
+    @Test
+    public void testRemoveAccount() {
+    }
+
+    /**
+     * Test of getAccount method, of class Customer.
+     */
+    @Test
+    public void testGetAccount() {
+    }
+
+    /**
+     * Test of toString method, of class Customer.
+     */
+    @Test
+    public void testToString() {
+    }
+
+    /**
+     * Test of hashCode method, of class Customer.
+     */
+    @Test
+    public void testHashCode() {
+    }
+
+    /**
+     * Test of equals method, of class Customer.
+     */
+    @Test
+    public void testEquals() {
+    }
+
+    /**
+     * Test of compareTo method, of class Customer.
+     */
+    @Test
+    public void testCompareTo() {
+    }
+    
+}
