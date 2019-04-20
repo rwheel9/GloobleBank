7f4b6b1b8619f98372b2ea57b3ab8c312a5f2c26
Reid Wheeler <rwheeler9@hawkmail.hccfl.edu>
2019-04-19 22:46:40 -0400

    Various changes and additions to BankTest.java

--- src/test/java/com/glooble/banking/BankTest.java
+++ src/test/java/com/glooble/banking/BankTest.java
@@ -14,17 +14,32 @@ import static org.junit.Assert.*;
  * @author Reid
  */
 public class BankTest {
+    private static final String JOHN = "John";
+    private static final String DOE = "Doe";
+    private Bank centralBank;
+    private Bank bank1;
+    Customer cust1;
     
     /**
      * Setting things up before running any test methods.
      */
     @BeforeClass
-    public static void setup() {
-        Bank bankTest = new Bank("bankTest");
-        String firstName = "Bob";
-        String lastName = "Smith";
-        Customer bobSmith = new Customer(bankTest, lastName, firstName);
+    public static void initSetup() {
+       //moved this setup to the @Before method below. 
+    }
     
+    @Before
+    public void setup() {
+        bank1 = new Bank("Bank1");
+        centralBank = new Bank("Central Bank");
+        cust1 = new Customer(centralBank, DOE, JOHN);
+    }
+    
+    @After
+    public void tearDown() {
+        bank1 = null;
+        centralBank = null;
+        cust1 = null;
     }
     
     public BankTest() {
@@ -64,14 +79,13 @@ public class BankTest {
 
     /**
      * Test of addCustomer method, of class Bank.
-     * Tests if the customerId output is correct.
+     * Tests if the customerId output is correct. Note: as mentioned in the
+     * assignment description, this test assumes all customer names are unique.
      */
     @Test
-    public void testAddCustomer() {
-        final Bank bank1 = new Bank("Bank1");
-        final Customer cust1 = new Customer(bank1, "Doe", "John");
+    public void testAddCustomer_existingCustomer() {
         String expectedResult = cust1.getCustomerId();
-        String result = bank1.addCustomer("Doe", "John");
+        String result = bank1.addCustomer(DOE, JOHN);
         assertEquals("Customer ID should be \"" + expectedResult + "\" but was \""
                 + result + "\".", expectedResult, result);
         assertNotNull("Customer ID should not be null.", result);
@@ -79,19 +93,48 @@ public class BankTest {
 
     /**
      * Test of the addCustomer method.
-     * Tests if a Customer object was added to the specified Bank.
+     * Tests the behavior of adding a customer without preexisting Customer object.
      */
     @Test
-    public void testAddCustomer_actuallyAdded() {
-        final Bank bank1 = new Bank("Bank1");
-        // To be continued or deleted
+    public void testAddCustomer_newCustomer() {
+        String result = bank1.addCustomer("Bob", "Jimmy");
+        assertNotNull(result);
+        
     }
     
     /**
      * Test of removeCustomer method, of class Bank.
+     * Tests whether the Customer object is removed from the bank.
      */
     @Test
     public void testRemoveCustomer() {
+        String customerId = cust1.getCustomerId();
+        centralBank.removeCustomer(customerId);
+        Customer result = centralBank.getCustomer(customerId);
+        assertNotSame(cust1, result);
+        
+    }
+    
+    /**
+     * Test of removeCustomer method to ensure the Customer object is not deleted
+     * entirely, but only removed from the bank's listings.
+     */
+    @Test
+    public void testRemoveCustomer_customerStillExists() {
+        centralBank.removeCustomer(cust1.getCustomerId());
+        assertNotNull("Removal from the bank should not delete the customer object.", cust1);
+    }
+    
+    /**
+     * Test of the removeCustomer method with the addCustomer method supplying 
+     * the argument. This is not something I would expect to occur naturally; 
+     * I am just trying things at this point.
+     * 
+     */
+    @Test
+    public void testRemoveAddedCustomer_whatWillThisDo() {
+        //bank1.removeCustomer(bank1.addCustomer(DOE, JOHN));
+        //assertNull(bank1.getCustomer(cust1.getCustomerId()));
     }
 
     /**
